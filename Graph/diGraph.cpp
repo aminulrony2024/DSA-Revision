6b83d1430685d019857b05a6765c19aa4b01161e
aminulrony2024 <aminulrony2024@gmail.com>
2025-04-03 21:39:24 +0530

    BFS traversal done

--- Graph/diGraph.cpp
+++ Graph/diGraph.cpp
@@ -1,45 +1,59 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-void bfs(int &V, vector<int> adj[], vector<int> &vis)
+void bfs(int x, vector<int> adj[], vector<int> &vis)
 {
     queue<int> q;
-    int x;
-    cout<<"Enter the starting vertex : ";
-    cin>>x;
     q.push(x);
     vis[x] = 1;
-    cout<<"BFS Traversal of the graph : "<< x <<" ";
-    while(!q.empty())
+    cout << "BFS Traversal of the graph : " << x << " ";
+    while (!q.empty())
     {
         int node = q.front();
         q.pop();
-        for(auto &it : adj[node])
+        for (auto &it : adj[node])
         {
-            if(vis[it] != 1)
+            if (vis[it] != 1)
             {
                 vis[it] = 1;
                 q.push(it);
-                cout<<it<<" ";
+                cout << it << " ";
             }
         }
     }
 }
+void dfs(int x, vector<int> adj[], vector<int> &vis)
+{
+    vis[x] = 1;
+    cout << x << " ";
+    for (auto &it : adj[x])
+    {
+        if (vis[it] != 1)
+            dfs(it, adj, vis);
+    }
+}
 int main()
 {
-    int V,E ;
-    cout<<"Enter the number of vertices: ";
-    cin>>V;
-    cout<<"Enter the number of Edges : ";
-    cin>>E;
-    vector<int> adj[V+1];
-    cout<<"Enter the edges : " <<endl;
-    for(int i = 0; i < E; i++)
+    int V, E;
+    cout << "Enter the number of vertices: ";
+    cin >> V;
+    cout << "Enter the number of Edges : ";
+    cin >> E;
+    vector<int> adj[V + 1];
+    cout << "Enter the edges : " << endl;
+    for (int i = 0; i < E; i++)
     {
-        int u,v;
-        cin>>u>>v;
+        int u, v;
+        cin >> u >> v;
         adj[u].push_back(v);
     }
-    vector<int> vis(V+1,0);
-    bfs(V,adj,vis);
+    vector<int> vis(V + 1, 0);
+    cout << "Enter the starting vertex: ";
+    int x;
+    cin >> x;
+    bfs(x, adj, vis);
+    cout << "\nDFS Traversal of the Graph : ";
+    for(auto &it : vis)
+    it = 0;
+    dfs(x, adj, vis);
     return 0;
 }
